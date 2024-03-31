da0445092ba82f8162deb30f8ce35d9d3aa9e14a
mattiazin <164552367+mattiazin@users.noreply.github.com>
2024-03-31 15:33:57 -0300

    Add files via upload

---
+++ fund/lista2_Respostas /4/main (3).cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Quadrado:" << std::endl;
+    std::cout << "*********" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << "*********" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Círculo/Oval:" << std::endl;
+    std::cout << "  ***  " << std::endl;
+    std::cout << " *   * " << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << "*     *" << std::endl;
+    std::cout << " *   * " << std::endl;
+    std::cout << "  ***  " << std::endl;
+    std::cout << std::endl;
+
+
+    std::cout << "Seta:" << std::endl;
+    std::cout << "  *  " << std::endl;
+    std::cout << " *** " << std::endl;
+    std::cout << "*****" << std::endl;
+    std::cout << "  *  " << std::endl;
+    std::cout << "  *  " << std::endl;
+    std::cout << "  *  " << std::endl;
+    std::cout << "  *  " << std::endl;
+    std::cout << "  *  " << std::endl;
+    std::cout << "  *  " << std::endl;
+
+
+    std::cout << "Losango:" << std::endl;
+    std::cout << "    *    " << std::endl;
+    std::cout << "   * *   " << std::endl;
+    std::cout << "  *   *  " << std::endl;
+    std::cout << " *     * " << std::endl;
+    std::cout << "*       *" << std::endl;
+    std::cout << " *     * " << std::endl;
+    std::cout << "  *   *  " << std::endl;
+    std::cout << "   * *   " << std::endl;
+    std::cout << "    *    " << std::endl;
+
+    return 0;
+}
