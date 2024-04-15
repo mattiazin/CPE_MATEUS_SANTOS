42bfbbddaab21e2d386120d12ecf143c665fc14c
mattiazin <164552367+mattiazin@users.noreply.github.com>
2024-04-15 14:34:47 -0300

    Add files via upload

---
+++ func/lista5_Respostas /02 /A)/main (2).cpp
@@ -0,0 +1,13 @@
+bool ehPrimo(int num) {
+  if (num <= 1) {
+    return false;
+  }
+
+  for (int i = 2; i <= sqrt(num); i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
